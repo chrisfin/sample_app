65af266838fed0f040ab8b6f731353e77336051e
Chris Finlayson <chrisfinlayson@gmail.com>
2014-02-20 13:26:48 -0500

    Finish user CRUD

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,6 +16,9 @@ def current_user=(user)
     @current_user ||= User.find_by(remember_token: remember_token)
   end
 
+def current_user?(user)
+    user == current_user
+  end
 
 def signed_in?
     !current_user.nil?
@@ -24,6 +27,15 @@ def signed_in?
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end 
+
+  def store_location
+    session[:return_to] = request.url if request.get?
   end
 
 end
